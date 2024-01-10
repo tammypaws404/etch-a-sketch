261942007d95b5dd4f6b56f1f1b4e56663ca351b
Tammy <tammyle.uc@gmail.com>
2024-01-10 20:57:02 +1100

    Fix grid adapting to different sizes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector('#gridContainer');
 function makeGrid(size=16) {
     for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
+        row.classList.add('row');
         for (let i = 0; i < size; i++) {
             const square = document.createElement('div');
             square.classList.add('square')
@@ -11,11 +12,15 @@ function makeGrid(size=16) {
         container.appendChild(row);
     }
 
-    // same total space (900px) for any size input
+    // same total space (960px) for any size input
     const squares = document.querySelectorAll('.square');
-    containerWidth = 900;
+    const containerWidth = 600;
     for (const square of squares) {
-        square.style.padding = `${(containerWidth / size) / 2}px`
+        square.style.width = `${containerWidth / size}px`;
+        square.style.height = `${containerWidth / size}px`;
+        // border-box fixed everything
+        square.style.boxSizing = 'border-box';
+
     }    
 
     // when square hovered, change color
@@ -28,7 +33,11 @@ function makeGrid(size=16) {
 
 const sizeBtn = document.querySelector('#sizeBtn');
 sizeBtn.addEventListener('click', () => {
-    const size = prompt('Squares per side');
+    // limit user input to 100
+    let size;
+    while (size === undefined || size > 100) {
+        size = prompt('Squares per side');
+    }
     container.textContent = '';
     makeGrid(size);
 })
